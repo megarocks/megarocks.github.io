fa3fc85c860d88a54883d628ff0955867c766816
Vitaly <vitaly.kiktiov@gmail.com>
2018-02-23 07:45:50 +0200

    wrap empty canvas using angular component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('mainCanvas') mainCanvas: ElementRef;
+  public context: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    this.context = (<HTMLCanvasElement>this.mainCanvas.nativeElement).getContext('2d');
+  }
 }
